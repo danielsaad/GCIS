f902e07e92cded2d3ce95340f7226ab94a308c60
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2018-06-22 01:59:05 -0300

    updatind run-decode-saca script

--- scripts/run-decode-saca.py
+++ scripts/run-decode-saca.py
@@ -10,6 +10,78 @@ class experiment_data:
         self.experiment_time = []
         self.experiment_input = []
 
+
+def run_decode_nong(input_folder_path,output_folder_path):
+    e = experiment_data()
+    e.experiment_name = "DECODE-SAIS-NONG"
+    input_filename = os.listdir(input_folder_path)
+    for f in input_filename:
+        input_file = os.path.join(input_folder_path,f)
+        output_file = os.path.join(output_folder_path,f+'.sanong')
+        compressed_file = os.path.join(output_folder_path,f+'.gcis')
+        decompressed_file = os.path.join(output_folder_path,f)
+
+        print("GCIS Compressing: ",f)
+        process = subprocess.run(['../bin/gc-is-codec','-c',input_file,compressed_file],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+        print("GCIS Decoding")
+        start_time = time.perf_counter() # Start time
+        process = subprocess.run(['../bin/gc-is-codec','-d',compressed_file,decompressed_file],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        end_time = time.perf_counter() # End time
+        print("Time Decode =","{:.2f}".format(end_time-start_time))
+        total_time = end_time - start_time
+
+
+        print("SACA Nong: ",f)
+        start_time = time.perf_counter() # Start time
+        process = subprocess.run(['../bin/sais-nong', '-c', decompressed_file,output_file],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        end_time = time.perf_counter() # End time
+        total_time += end_time - start_time
+        print("Time Decode + Nong =","{:.2f}".format(total_time))
+        e.experiment_input.append(f)
+        e.experiment_time.append("{:.2f}".format(end_time-start_time))
+
+    return e
+
+def run_decode_yuta(input_folder_path,output_folder_path):
+    e = experiment_data()
+    e.experiment_name = "DECODE-SAIS-YUTA"
+    input_filename = os.listdir(input_folder_path)
+    for f in input_filename:
+        input_file = os.path.join(input_folder_path,f)
+        output_file = os.path.join(output_folder_path,f+'.sayuta')
+        compressed_file = os.path.join(output_folder_path,f+'.gcis')
+        decompressed_file = os.path.join(output_folder_path,f)
+
+        print("GCIS Compressing: ",f)
+        process = subprocess.run(['../bin/gc-is-codec','-c',input_file,compressed_file],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+        print("GCIS Decoding")
+        start_time = time.perf_counter() # Start time
+        process = subprocess.run(['../bin/gc-is-codec','-d',compressed_file,decompressed_file],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        end_time = time.perf_counter() # End time
+        print("Time Decode =","{:.2f}".format(end_time-start_time))
+        total_time = end_time - start_time
+
+
+        print("SACA Yuta: ",f)
+        start_time = time.perf_counter() # Start time
+        process = subprocess.run(['../bin/sais-yuta', '-c', decompressed_file,output_file],
+        stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        end_time = time.perf_counter() # End time
+        total_time += end_time - start_time
+        print("Time Decode + Yuta =","{:.2f}".format(total_time))
+        e.experiment_input.append(f)
+        e.experiment_time.append("{:.2f}".format(end_time-start_time))
+
+    return e
+
+
 def run_sais_nong(input_folder_path,output_folder_path):
     e = experiment_data()
     e.experiment_name = "SAIS-NONG"
@@ -85,10 +157,12 @@ def run_saca(input_folder_path,output_folder_path):
     decode_saca_data = run_decode_saca(input_folder_path,output_folder_path)
     saca_nong_data = run_sais_nong(input_folder_path,output_folder_path)
     saca_yuta_data = run_sais_yuta(input_folder_path,output_folder_path)
+    decode_saca_nong_data = run_decode_nong(input_folder_path,output_folder_path)
+    decode_saca_yuta_data = run_decode_yuta(input_folder_path,output_folder_path)
  
     with open(os.path.join(output_folder_path,'results-saca.csv'),'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
-        header = ['Input File',decode_saca_data.experiment_name,saca_nong_data.experiment_name,saca_yuta_data.experiment_name]
+        header = ['Input File',decode_saca_data.experiment_name,saca_nong_data.experiment_name,saca_yuta_data.experiment_name,decode_saca_nong_data,decode_saca_yuta_data]
         writer.writerow(header)
         time = list(zip(decode_saca_data.experiment_time,saca_nong_data.experiment_time,saca_yuta_data.experiment_time))
         for i,t in enumerate(time):
